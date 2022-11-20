de119d8d5f2f66d41e5600667f0baf792deaccd6
anandstephan <anand9412868527@gmail.com>
2022-11-20 17:50:06 +0530

    feat: adding ChannelCard and VideoCard

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ import { categories } from '../utils/constants'
 
 const selectedCategory = 'New'
 
- const Sidebar = () => {
+ const Sidebar = ({selectedCategory,setSelectedCategory}) => {
   return (
     <Stack direction="row"
     sx={{
@@ -17,7 +17,7 @@ const selectedCategory = 'New'
             <button className='category-btn' style={{
                 background:category.name === selectedCategory &&'#FC1503',
                 color:'white'
-            }}>
+            }} key={category.name} onClick={()=>setSelectedCategory(category.name)}>
                 <span style={{color:category.name === selectedCategory ? 'white':"red",marginRight:'15px'}}>{category.icon}</span>
                 <span style={{opacity:category.name === selectedCategory ? 1:0.8}}>{category.name}</span>
             </button>
